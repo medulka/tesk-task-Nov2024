83596648a07a015ca1c5faa6bafdddc1488819ca
medulka <hanka.medova@gmail.com>
2024-11-30 08:41:08 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+README.md
+
+A simple scenario was taken into the consideration. A participant from one framework wishes to operate in the second framework. Would his credentials be transferable?
+
+The snippet aims to answer the question: "Which standards do your credentials follow?" 
+
+Distinction between legal/natural person and the event type were neglected. 
+
+Data sources:
+
+https://i4trust.github.io/building-blocks/docs/i4Trust-BuildingBlocks_v4.0.pdf
+
+page 65 and 164
+
+https://ces-development.lab.gaia-x.eu/q/swagger-ui/
+
---
+++ analysis-1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import json
+import re
+from datetime import datetime
+
+# with open(file="/Users/hanamedova/Documents/Gaia-X/building-blocks/test-task/i4Trust-OIDC.json") as f:
+#     data = json.load(f)
+    
+# with open(file="/Users/hanamedova/Documents/Gaia-X/building-blocks/test-task/GX-swagger-example.json") as f:
+#     data = json.load(f)
+
+with open(file="/Users/hanamedova/Documents/Gaia-X/building-blocks/test-task/i4Trust-DID.json") as f:
+    data = json.load(f)    
+
+for i in data:
+    if i in ['iss', 'issuer']:
+        if data[i].startswith("EU.EORI"):
+            print("An OIDC identifier. The participant ID cannot be recognised. Please provide DID credentials.")
+    if i == "jti":
+        print("signed by JSON Web Token")
+    if i == "data":
+        if data["data"]["issuer"].startswith("did:web"):
+            print("DID identifier found.")    
+        print(data["data"]["type"][0]) 
+        if print(data["data"]["proof"]["type"]).startswith("JsonWebSignature"):
+            print("JSON Web Signature used.")
+    if i == "payload":
+        if data["payload"]["id"].startswith("did:elsi"):
+            print("DID identifier found.") 
+        if data["payload"]["verificationMethod"][0]["type"].startswith("JsonWebKey"):
+            print("JSON Web Key used.") 
+    else: 
+        pass
+
+print("Standards check done. Parsing finished.")
+
+
+
+
+
